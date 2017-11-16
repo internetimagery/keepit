fac107ff30b9003ae1df0651d2a88c704912db39
Jason Dixon <jdixon@wetafx.co.nz>
2017-11-17 09:48:02 +1300

    save finaled?

--- save.py
+++ save.py
@@ -1,4 +1,5 @@
 # Save scene
+from __future__ import print_function
 import maya.cmds as cmds
 import maya.mel as mel
 
@@ -7,12 +8,13 @@ def kill(id_):
     if cmds.scriptJob(ex=id_):
         cmds.scriptJob(kill=id_)
 
-def Save(callback):
+def Save(callback, debug=False):
     """ Save scene! """
     job = cmds.scriptJob(e=('SceneSaved', callback), ro=True)
     cmds.scriptJob(e=('idle', lambda: kill(job)), ro=True)
     try:
-        mel.eval("checkForUnknownNodes(); FileMenu_SaveItem") # Perform save, using mayas built in save
+        mel.eval("SaveScene") # Perform save, using mayas built in save
+        # mel.eval("checkForUnknownNodes(); FileMenu_SaveItem") # Perform save, using mayas built in save
     except Exception as err:
         kill(job)
         raise err
@@ -20,26 +22,5 @@ def Save(callback):
 def test():
     """ Quick test """
     def say():
-        print "SAVE CALLBACK FIRED!!"
-
+        print("SAVE CALLBACK FIRED!!")
     Save(say)
-
-#
-#
-#         if os.path.isfile(cmds.file(q=True, sn=True)):  # Check the scene is not untitled and still exists
-#             process = cmds.scriptJob(e=['SceneSaved', performArchive], ro=True)
-#             try:
-#                 message = """
-# <div>- This Scene was last saved on <em>%(time)s</em>.</div>
-# <div>- Completing the task: <code>%(todo)s</code></div>
-# <div>- The file <strong>has not been modified since.</strong></div><br>
-# """ % {"time": time.ctime(), "todo": tempmeta["label"]}
-#                 with Popup(message):
-#                     cmds.file(save=True)  # Save the scene
-#             except RuntimeError:  # If scene save was canceled or failed. Reset everything
-#                 if cmds.scriptJob(ex=process):
-#                     cmds.scriptJob(kill=process)
-#                 s.data[uid] = temp
-#                 s._buidTodoTasks()
-#         else:
-#             performArchive()
