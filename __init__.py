3513df38764280c9461c2f15a977a0fa94300701
Jason Dixon <jdixon@wetafx.co.nz>
2017-11-21 11:42:59 +1300

    revert back to more complicated but robust save procedure

--- __init__.py
+++ __init__.py
@@ -6,8 +6,6 @@ import popup
 import view
 import save
 
-saver = save.Save(archive.archive)
-
 def keep():
     """ Perform an archive save! """
     # Ask for a message. If no message, save as normal without archive.
@@ -16,11 +14,10 @@ def keep():
         # Perform archive
         sup = popup.Startup(note)
         with sup:
-            saver.note = note
-            saver.save()
+            save.save_and_call(functools.partial(archive.archive, note))
     else:
         # Save normally. Thank you, come again!
-        saver.save()
+        save.save()
 
 def look():
     """ Look at what we have """
--- save.py
+++ save.py
@@ -3,23 +3,32 @@ from __future__ import print_function
 import maya.cmds as cmds
 import maya.mel as mel
 
-class Save(object):
-    """ Monitor scene saving """
-    def __init__(s, func):
-        s.func = func
-        s.note = ""
-        cmds.scriptJob(e=("SceneSaved", s.save_callback))
+def kill(id_):
+    """ Kill script job """
+    cmds.file(modified=False)
+    if cmds.scriptJob(ex=id_):
+        cmds.scriptJob(kill=id_)
 
-    def save_callback(s):
-        """ Triggered on scene save """
-        path = cmds.file(q=True, sn=True)
-        if path and s.note:
-            s.func(s.note, path)
-        s.note = ""
+def save():
+    """ Run maya's built in save """
+    if cmds.file(q=True, sn=True):
+        mel.eval("SaveScene")
+    else:
+        mel.eval("SaveSceneAs") # Use manually in case it's overriden
+    # mel.eval("checkForUnknownNodes(); FileMenu_SaveItem") # Perform save, using mayas built in save
 
-    def save(s):
-        """ Run maya's built in save """
-        if cmds.file(q=True, sn=True):
-            mel.eval("SaveScene")
-        else:
-            mel.eval("SaveSceneAs") # Use manually in case it's overriden
+def save_and_call(callback):
+    """ Save scene! """
+    job = cmds.scriptJob(e=('SceneSaved', lambda: callback(cmds.file(q=True, sn=True))), ro=True)
+    cmds.scriptJob(e=('idle', lambda: kill(job)), ro=True)
+    try:
+        save()
+    except Exception as err:
+        kill(job)
+        raise err
+
+def test():
+    """ Quick test """
+    def say():
+        print("SAVE CALLBACK FIRED!!")
+    save_and_call(say)
