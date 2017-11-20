9e9690f0a20c9389e593254eb7f172ab27ac8a40
Jason Dixon <jdixon@wetafx.co.nz>
2017-11-20 20:08:43 +1300

    fill the space!

--- view.py
+++ view.py
@@ -44,8 +44,8 @@ class Version(object):
             try:
                 s.index = json.loads(z.read("index.json"))
 
-                thumb = "<img width={} src='data:image/{};base64,{}'>".format(
-                    WIDTH, os.path.splitext(s.index["thumb"])[1][1:], base64.b64encode(z.read(s.index["thumb"])))
+                thumb = "<img height={} src='data:image/{};base64,{}'>".format(
+                    HEIGHT-30, os.path.splitext(s.index["thumb"])[1][1:], base64.b64encode(z.read(s.index["thumb"])))
                 desc = "created: {}\nnote: {}".format(datetime.datetime.fromtimestamp(s.index["time"]).strftime("%I:%M%p %a %d/%m/%Y"), s.index["note"])
                 if ucmds(cmds.text, img, q=True, ex=True):
                     ucmds(cmds.text, img, e=True, l=thumb, ann=desc)
